eefa076f9abdcb5f5567ee8c2e6df94afdb0f55f
britesc <julian@britesc.com>
2025-11-04 17:25:24 +0100

    First Commit

---
+++ ears_v200.ino
@@ -0,0 +1,67 @@
+/**
+ * @file ears_v200.ino
+ *
+ * @mainpage EARS Project
+ *
+ * @section description Description
+ * EARS - Equipment and Ammunition Reporting  System.
+ *
+ * @section libraries Libraries
+ * - GFX Library for Arduino (https://github.com/moononournation/Arduino_GFX)
+ *   - Graphics Gateway.
+ * - Widgets Library for Arduino (https://github.com/lvgl/lvgl)
+ *   - Widgets
+ *
+ * @section notes Notes
+ * - Comments are Doxygen compatible.
+ *
+ * @section todo TODO
+ * - Don't use Doxygen style formatting inside the body of a function.
+ *
+ * @section author Author
+ * - Created by JTB on 20251104.
+ * - Modified by by JTB on 20251104.
+ *
+ * Copyright (c) 2025 JTB.  All rights reserved.
+ */
+
+/**
+ * Core 0
+ */
+
+/**
+ * The standard Arduino setup function used for setup and configuration tasks.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/**
+ * The standard Arduino loop function used for repeating tasks.
+ */
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/**
+ * Core 1
+ */
+
+/**
+ * The standard Arduino setup function used for setup1 and configuration tasks.
+ */
+void setup1() {
+  // put your setup code here, to run once:
+
+}
+
+/**
+ * The standard Arduino loop1 function used for repeating tasks.
+ */
+void loop1() {
+  // put your main code here, to run repeatedly:
+
+}
